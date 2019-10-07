dc33b87ab2652f66472773c4cd3e5e383a07474d
Ritik Kumar <ritik.kumar_cs18@gla.ac.in>
2019-10-07 04:58:36 -1200

    Update Fibo.java

--- Fibo.java
+++ Fibo.java
@@ -5,6 +5,7 @@ public class Fibo{
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         int a = 0, b  = 1, c;
+        System.out.println("Enter the number upto which you want to print the series:-");
         int n = sc.nextInt();
         System.out.print(a +" "+b);
         for (int i = 2; i<n; i++){
