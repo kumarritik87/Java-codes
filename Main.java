e4c39295f14852001afae1b2c3677b768bed4130
Ritik Kumar <ritik.kumar_cs18@gla.ac.in>
2019-10-07 15:11:11 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,10 +1,7 @@
-/******************************************************************************
+/*java hello world program;
+@Author : - Ritik Kuamr;
 
-                            Online Java Compiler.
-                Code, Compile, Run and Debug java program online.
-Write your code in this editor and press "Run" button to execute it.
-
-*******************************************************************************/
+*/
 
 public class Main
 {
