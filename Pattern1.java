3eadee3deffeb858e119ea461469bc228f2fbc8e
Ritik Kumar <ritik.kumar_cs18@gla.ac.in>
2019-10-08 01:40:55 -1200

    Update Pattern1.java

--- Pattern1.java
+++ Pattern1.java
@@ -1,4 +1,14 @@
- public class Pattern1{
+/*  java program to print pattern of right angled triangle :- 
+*
+* *
+* * *
+* * * *
+* * * * *
+* * * * * * 
+* * * * * * *                                                */
+
+
+public class Pattern1{
     public static void main(String[] args){
         int i, j, n = 7;
         for(int a = 0; a<n; a++){
