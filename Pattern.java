b72040bc4c0334d003d6851546d263817136d94a
Ritik Kumar <ritik.kumar_cs18@gla.ac.in>
2019-10-08 01:20:26 -1200

    Add files via upload

---
+++ Pattern.java
@@ -0,0 +1,12 @@
+public class Pattern{
+    public static void main(String[] args){
+        int i, j, n = 4;
+        for(int a = 0; a<4; a++){
+            for(int b = 0; b<=a; b++){
+                System.out.print(""+(char)(65 + a));
+                
+            }
+            System.out.println("");
+        }
+    }
+}
