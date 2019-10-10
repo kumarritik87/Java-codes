d040f697573b8457b19cbf644fc5ad4ef4f36ffd
Ritik Kumar <ritik.kumar_cs18@gla.ac.in>
2019-10-10 21:02:36 +0530

    Update Pattern1.java

--- Pattern1.java
+++ Pattern1.java
@@ -8,13 +8,14 @@
 * * * * * * *                                                */
 
 
+
+
 public class Pattern1{
     public static void main(String[] args){
         int i, j, n = 7;
         for(int a = 0; a<n; a++){
             for(int b = 0; b<=a; b++){
                 System.out.print(" *");
-                
             }
             System.out.println("");
         }
